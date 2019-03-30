6398be723c8888ef0a9354b8847d905e7542e9a1
Zack Bloom <zack@cloudflare.com>
2019-03-30 14:58:22 -0500

    Update to new blog with Gatsby

---
+++ src/components/bio.js
@@ -0,0 +1,62 @@
+/**
+ * Bio component that queries for data
+ * with Gatsby's StaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/static-query/
+ */
+
+import React from "react"
+import { StaticQuery, graphql } from "gatsby"
+import Image from "gatsby-image"
+
+import { rhythm } from "../utils/typography"
+
+function Bio() {
+  return (
+    <StaticQuery
+      query={bioQuery}
+      render={data => {
+        const { author, social } = data.site.siteMetadata
+        return (
+          <div
+            style={{
+              display: `flex`,
+              marginBottom: rhythm(2.5),
+            }}
+          >
+            <p>
+              I work at Cloudflare in San Francisco where I help build
+              the future of the Internet.
+              &nbsp;
+              <a href={`https://twitter.com/${social.twitter}`}>
+                Follow me on Twitter
+              </a>
+            </p>
+          </div>
+        )
+      }}
+    />
+  )
+}
+
+const bioQuery = graphql`
+  query BioQuery {
+    avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
+      childImageSharp {
+        fixed(width: 50, height: 50) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+    site {
+      siteMetadata {
+        author
+        social {
+          twitter
+        }
+      }
+    }
+  }
+`
+
+export default Bio
---
+++ src/components/layout.js
@@ -0,0 +1,79 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import { rhythm, scale } from "../utils/typography"
+
+class Layout extends React.Component {
+  render() {
+    const { location, title, children } = this.props
+    const rootPath = `${__PATH_PREFIX__}/`
+    let header
+
+    if (location.pathname === rootPath) {
+      header = (
+        <h1
+          style={{
+            ...scale(1.5),
+            marginBottom: rhythm(1.5),
+            marginTop: 0,
+            fontSize: `1.875em`,
+            fontFamily: `Futura, sans-serif`,
+            textTransform: `uppercase`
+          }}
+        >
+          <Link
+            style={{
+              boxShadow: `none`,
+              textDecoration: `none`,
+              color: `inherit`,
+            }}
+            to={`/`}
+          >
+            {title}
+          </Link>
+        </h1>
+      )
+    } else {
+      header = (
+        <h3
+          style={{
+            fontFamily: `Futura, sans-serif`,
+            textAlign: `center`,
+            fontSize: `1em`,
+            textTransform: `uppercase`,
+            letterSpacing: `.25em`,
+            textIndent: `.25em`,
+            paddingTop: `3.75em`,
+            margin: `0 auto 1.5em`,
+          }}
+        >
+          <Link
+            style={{
+              boxShadow: `none`,
+              textDecoration: `none`,
+              color: `inherit`,
+            }}
+            to={`/`}
+          >
+            {title}
+          </Link>
+        </h3>
+      )
+    }
+    return (
+      <div
+        style={{
+          marginLeft: `auto`,
+          marginRight: `auto`,
+          maxWidth: rhythm(24),
+          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+        }}
+      >
+        <header>{header}</header>
+        <main>{children}</main>
+      </div>
+    )
+  }
+}
+
+export default Layout
